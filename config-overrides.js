97b607d5650e43f72a96e451b6ad52cf0bda61f6
RyoLinEth <ryolindev@gmail.com>
2023-05-12 19:43:08 +0800

    config-override

---
+++ config-overrides.js
@@ -0,0 +1,17 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = function override(config, env) {
+  // 添加壓縮和混淆插件
+  config.plugins.push(
+    new CompressionPlugin({
+      test: /\.(js|css|html|svg|xml|json)$/,
+      filename: '[path][base].gz',
+      algorithm: 'gzip',
+      threshold: 10240,
+      minRatio: 0.8,
+    })
+  );
+  
+  // 返回修改後的配置
+  return config;
+};
